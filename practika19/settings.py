50618f30c240ad0f1fc4a085eb9190f95756fee4
Aleksandr K <zidan.90@yandex.ru>
2023-03-25 17:28:34 +0300

    Praktika19

---
+++ practika19/settings.py
@@ -0,0 +1,9 @@
+valid_email = "zidan.90@yandex.ru"
+valid_password = "Zidan19901802"
+
+valid_email2 = "anehka-8888@mail.ru"
+valid_password2 = "Qwerty123"
+
+invalid_email = "anehka123@mail.ru"
+invalid_password = "Qwerty12345"
+invalid_key = {'key': "00000"}
